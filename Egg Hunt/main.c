dfd21bf9e0b64201f6dfeea97fc6ada33f222869
Paul Senatillaka <paulsena@gmail.com>
2011-11-09 19:57:37 -0500

    Fix so we ignore certain files

---
+++ Egg Hunt/main.c
@@ -0,0 +1,296 @@
+/*********************************
+* Paul Senatillaka
+* Ramon Ramirez
+* Egg Hunt
+
+**State machine**
+States: Collect -> FindGoal -> Dump
+                  
+Note: Camera Pixel Dimensions: 159 x 119
+
+Todo:
+-Fix finding home from opponents side
+-Stay away from light when not going home
+-Better search when no eggs seen. Currently just spins.
+-When spinning, read side dist sensor to decide which way to turn
+-Add code for front closer
+-Readjust 180 turn. Actually don't use discrete turns/positioning.
+-Add side calibration
+**********************************/
+#define DEBUG
+
+#include "helper.h";
+
+//**** Variables ****
+//Corrosponds to Top Hat Digital Reading
+//White Side = 1, Dark Side = 0
+const int homeColor = 0; 
+const int awayColor = 1; 
+enum COLOR ourEggColor = PURPLE;
+const int numBallsCollect = 3;
+//*******************
+
+
+int main()
+{
+	
+	//Init Variables
+	float startTime = seconds();
+	enum STATE state = COLLECT;
+
+	enum DIRECTION lastSeen = forward;
+	int ballCount = 0;
+	
+	//Set our Distance Sensors to Float	
+	set_each_analog_state(0,0,1,1,0,0,0,0);
+	
+	//Clears our track motor position.
+	//Note: Pusher should always be set in the back position on start
+	clear_motor_position_counter(trackMotor);
+	
+	//Possible Initial Vision Planning
+	
+	while(!black_button()) {
+			
+		
+		sideDistCheckMove();
+		
+		bumperChecknMove();
+		
+		
+		switch(state) {
+			
+		//**** Collection State *****
+		//***************************
+		case COLLECT:
+			
+			//Image Capture
+			track_update();
+			
+			
+			//Egg Detect
+			if(track_count(ourEggColor) > 0 && track_confidence(ourEggColor, 0) > 40 ) {
+				
+				#ifdef DEBUG
+					printf("See our egg %d \n", track_x( ourEggColor, 0));
+				#endif
+				
+				//Find out where blobs are
+				//Right
+				if( track_x(ourEggColor, 0) > 115) {
+					move(diagRight, 800);
+					lastSeen = right;
+				}
+				//Left
+				else if( track_x(ourEggColor, 0) < 85 ) {
+					move(diagLeft, 800);
+					lastSeen = left;
+				}
+				//Forward
+				else {
+					move(forward, 800);
+										
+				}
+				
+				//Check if ball is about to go in. 
+				if( track_bbox_top(ourEggColor, 0) > 100) {
+					//TODO: OPEN GATE
+						
+					sleep(.5);
+					ballCount++;
+					beep();
+					
+					#ifdef DEBUG
+					printf("Ball In: %d\n", ballCount);
+					#endif
+				}
+				
+			}
+				
+			//We didn't find a blob
+			else {
+				
+				#ifdef DEBUG
+					printf("No Egg \n");
+				#endif
+				
+				//Move towards last seen direction
+				move(lastSeen, maxVelocity);
+				sleep(.5);
+				
+			}
+
+			//TODO:
+			//Obstacle Avoidance - Walls/Robots
+			
+			if (ballCount >= numBallsCollect) {
+				state = FINDGOAL;
+				ballCount = 0;
+			}
+			
+			break;
+
+		//***** Goal Finding State ******
+		//*******************************
+		case FINDGOAL:
+		
+			#ifdef DEBUG
+				printf("STATE: FindGoal \n");
+			#endif
+		
+			//Spin until either light sensor hits threshold
+			while ( analog(lLight) > lightThreshHigh && analog(rLight) > lightThreshHigh ) {
+				//look for the brightest light
+				move(pointLeft, maxVelocity);
+			}	
+					
+			//Read Ground Value
+			//If we are on our Home Side
+			if ( digital(groundSensor) == homeColor) {
+				
+				//Drive towards Light
+				move(forward, maxVelocity);
+				sleep(1);
+				
+				
+				//Recheck our ground value to make sure we're moving towards correct goal
+				if ( digital(groundSensor) == homeColor ) {
+					//Advance to our next state
+					state = DRIVEGOAL;
+				}
+				//We're heading towards the wrong goal
+				else {
+					#ifdef DEBUG
+					printf("We found and moved towards light, but on wrong side \n");
+					#endif
+					//Spin 180
+					move(oneEighty, maxVelocity);
+					
+					move(forward, maxVelocity);
+					sleep(3);
+				}
+				
+				
+			}
+			//We're in Oponent's Zone
+			else {
+				
+				//Rotate away from Light
+				move(oneEighty, maxVelocity);
+				
+				//Drive away from Light
+				move(forward, maxVelocity);
+				sleep(5);
+				
+				//Recheck our ground value to make sure we're moving away from opponents goal
+				if ( digital(groundSensor) == homeColor ) {
+					//Advance to our next state
+					state = DRIVEGOAL;
+				}
+				
+				
+			}
+		
+		
+			//Drive
+			
+			//Goal + Side Detection
+			
+			//Obstacle Avoidance - Walls/Robots
+			
+			
+			
+			break;
+			
+		//***** Drive towards Goal State ******
+		//*************************************
+		case DRIVEGOAL:
+			
+			#ifdef DEBUG
+				printf("STATE: DRIVEGOAL \n");
+			#endif
+			
+			//Good, we're still driving towards our Goal
+			if ( digital(groundSensor) == homeColor) {
+				
+				int leftValue = analog(lLight);
+				int rightValue = analog(rLight);	
+				
+				//Left Sensor Brighter
+				if ( leftValue < rightValue - 20 ) {
+					move(pointLeft, maxVelocity);
+				}
+				//Right Sensor Brighter
+				else if ( rightValue < leftValue - 20) {
+					move(pointRight, maxVelocity);
+				}
+				//Move Forwards
+				else {
+					if (leftValue < lightThreshLow || rightValue < lightThreshLow ) {
+						ao();
+						state = DUMP;
+					}
+					else {
+						move(forward, maxVelocity);
+					}
+				}
+				
+			}
+			//We need to turn around, heading towards wrong light source
+			else {
+				#ifdef DEBUG
+					printf("Wrong Side, Turn around \n");
+				#endif
+				//TODO: Calibrate is this turn is a 180
+				move(oneEighty, maxVelocity);
+				move(forward, maxVelocity);
+				sleep(3);
+				state = FINDGOAL;
+			}
+			
+			
+			break;
+		
+			
+		//**** Dumping State *****
+		//************************
+		case DUMP:
+			
+		#ifdef DEBUG
+			printf("STATE: Dump\n");
+		#endif
+		
+		ao();
+		
+		//If track isn't in back position, move it
+		if(!digital(trackLimit)) {
+				mav (trackMotor, -800);
+				while (!digital(trackLimit)) {};
+				mav (trackMotor, 0);
+				clear_motor_position_counter(trackMotor);	
+		}
+		
+		int i;
+		
+		for (i = 0; i<2; i++) {
+			//Lets Dump
+			move_to_position (trackMotor, 900, 2350);
+			sleep (3);
+			mav (trackMotor, -900);
+			while (!digital(trackLimit)) {};
+			mav (trackMotor, 0);
+			clear_motor_position_counter(trackMotor);
+		}
+		
+		//Dump finished, move back and turn
+		move(back, maxVelocity);
+		sleep(1);
+		move(oneEighty, maxVelocity);
+		
+		
+		state = COLLECT;
+		break;
+
+		}	
+	}
+}
